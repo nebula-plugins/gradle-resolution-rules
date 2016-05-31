3938c60cf8bdc93c489603757a6e99d0dc489128
Danny Thomas <dannyt@netflix.com>
2016-05-31 16:31:41 -0700

    Split replace-oss into individual rules

---
+++ src/main/resources/replace-google-collections.asm
@@ -0,0 +1,15 @@
+{
+    "replace" : [
+            {
+                "module" : "com.google.collections:google-collections",
+                "with" : "com.google.guava:guava",
+                "reason" : "Google Collections was superceded by Guava",
+                "author" : "Danny Thomas",
+                "date" : "2016-04-26T00:00:00.000Z"
+            }
+    ],
+    "align": [],
+    "substitute": [],
+    "deny": [],
+    "reject": []
+}
