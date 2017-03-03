5694edab6afa4f5309a6d72f5b2c421257d0c13f
Steve Hill <sghill.dev@gmail.com>
2017-03-02 16:24:58 -0800

    Allow .pr1 releases for jackson

--- src/test/groovy/VerifyRulesSpec.groovy
+++ src/test/groovy/VerifyRulesSpec.groovy
@@ -27,4 +27,32 @@ class VerifyRulesSpec extends IntegrationSpec {
         assert rulesFiles.size() > 0
         result.standardOutput.contains('a dependency rules source')
     }
+
+    def 'jackson pr1'() {
+        def rulesDir = new File('src/main/resources').absoluteFile
+        def rulesFiles = rulesDir.list()
+
+        buildFile << """
+        apply plugin: 'java'
+        apply plugin: 'nebula.resolution-rules'
+
+        repositories {
+            jcenter()
+        }
+
+        dependencies {
+            compile 'com.fasterxml.jackson.core:jackson-core:2.9.0.pr1'
+            compile 'com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.8.6'
+
+            resolutionRules fileTree('$rulesDir').include('align-jackson.json')
+        }
+        """
+
+        when:
+        def result = runTasksSuccessfully('dependencies', '--configuration', 'compile')
+
+        then:
+        assert rulesFiles.size() > 0
+        result.standardOutput.contains('com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.8.6 -> 2.9.0.pr1')
+    }
 }
